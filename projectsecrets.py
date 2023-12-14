24c178e43dda34d393f69b9b439326bac877587e
Chkjaer <114953470+Chkjaer@users.noreply.github.com>
2023-12-14 15:37:54 -0800

    Update projectsecrets.py

--- projectsecrets.py
+++ projectsecrets.py
@@ -1,11 +1,4 @@
 #secret keys
 import secrets
-<<<<<<< HEAD
-unsplash_access_key ="I6QDvkvGzBlTeQ26u4CiMcGRjXD2v9-ka9KbXBFPOrI"
-unsplash_secret='ojJLUnvy1Le4KbCtnkB5h-XRcKrlbKFJoSLrvSAmrSU'
-spotify_client_id='32f0b3c9bc8c4420a5ca7c7e2ea369ef'
-spotify_client_secret='ff07d4d8aa3b4ceb97a36a2311867bc3'
 secret_key = secrets.token_bytes(16)
-=======
-secret_key = secrets.token_bytes(16)
->>>>>>> f3209d074e378d0674ba469072a75d3a373f8f88
+
