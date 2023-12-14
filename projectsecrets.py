4d06151b0586bd85cd279e56d06d6e467825ec5f
Chkjaer <chkjaer@uw.edu>
2023-12-14 14:15:09 -0800

    new version

--- projectsecrets.py
+++ projectsecrets.py
@@ -1,2 +1,6 @@
 import secrets
 secret_key = secrets.token_bytes(16)
+unsplash_access_key ="I6QDvkvGzBlTeQ26u4CiMcGRjXD2v9-ka9KbXBFPOrI"
+unsplash_secret='ojJLUnvy1Le4KbCtnkB5h-XRcKrlbKFJoSLrvSAmrSU'
+spotify_client_id='32f0b3c9bc8c4420a5ca7c7e2ea369ef'
+spotify_client_secret='ff07d4d8aa3b4ceb97a36a2311867bc3'
